9be8bc62c51b0afd6901227ee10ea2f65b5b6fb8
RadFist <m.rifansyah.a@gmail.com>
2024-03-07 10:57:17 +0700

    first commit

---
+++ component/sidebar.php
@@ -0,0 +1,41 @@
+<div class="sidebar">
+        <div class="logo">
+            <ul class="menu">
+                <li class="active">
+                    <a href="#">
+                        <i class="fas fa-tachometer-alt"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-user"></i>
+                        <span>Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-chart-bar"></i>
+                        <span>Statistik</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-briefcase"></i>
+                        <span>Fitur</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-question-circle"></i>
+                        <span>Bantuan</span>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <span class="nav-item">Log out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+include 'component/connection.php';
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title> Dashboard | Retail</title>
+    <link rel="stylesheet" href="style.css" />
+    <!-- Font Awesome Cdn Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+
+<body>
+    <?php 
+    include 'component/sidebar.php';
+    ?>
+
+    
+    <div class="main--content">
+    
+    <?php 
+    include 'component/header.php';
+    ?>
+
+        <!-- card -->
+        <div class="card--container">
+            <h3 class="main--title">Data</h3>
+            <div class="card--wrapper">
+                <div class="payment--card dark-red">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">1</span>
+                            <span class="amount-value">Produk</span>
+                        </div>
+                        <i class="fas fa-archive"></i>
+                    </div>
+                    <span class="card-detail">_________</span>
+                </div>
+
+                <div class="payment--card dark-green">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">2</span>
+                            <span class="amount-value">Supplier</span>
+                        </div>
+                        <i class="fas fa-cart-plus"></i>
+                    </div>
+                    <span class="card-detail">_________</span>
+                </div>
+
+                <div class="payment--card dark-blue">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">3</span>
+                            <span class="amount-value">Karyawan</span>
+                        </div>
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <span class="card-detail">_________</span>
+                </div>
+
+                <div class="payment--card dark-purple">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">4</span>
+                            <span class="amount-value">Stock Barang</span>
+                        </div>
+                        <i class="fas fa-book"></i>
+                    </div>
+                    <span class="card-detail">_________</span>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="card--container" >
+            <h3 class="main--title">Statistik</h3>  
+        </div>
+
+        
+    </div>
+</body>
+
+</html>
